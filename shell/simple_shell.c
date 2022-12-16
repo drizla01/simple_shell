341673fdbfc52a1029e9f46d60f89042c4369cd3
awal umar <awalumar277@gmail.com>
2022-12-15 16:54:11 -0800

    put wait

--- shell/simple_shell.c
+++ shell/simple_shell.c
@@ -23,4 +23,36 @@ int main()
 		if (line_buffer[i] == '\n')
 		break;
 	}
+
+	free(line_buffer);
+
+	char *arguments[] = {arg, NULL};
+
+	int fork_ntimes = 1;
+
+	for ( int fork_count = 0; fork_ntimes; fork_count++)
+	{
+		printf("FORK number %d!\n", fork_count + 1);
+		child_pid = fork();
+
+
+		if (child_pid == -1)
+		{
+			perror("Failed to create child process");
+			return (1);
+		}
+		else if (child_pid > 0)
+		{
+			puts("WAITS");
+			wait(&wait_status);
+			puts("RESUMED FROM WAIT");
+		}
+
+		else if (child_pid == 0)
+		{
+			puts("I am forked child, to execute another program now");
+
+			execve(arguments[0], arguments, NULL);
+		}
+	}
 }
