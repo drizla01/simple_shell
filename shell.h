29ca6e35f43bd59f8e8ed9d220f728d813bfb858
awal umar <awalumar277@gmail.com>
2022-12-30 09:51:25 -0800

    header

--- shell.h
+++ shell.h
@@ -100,3 +100,18 @@ typedef struct liststr
  	0, 0, 0}
 
 
+/**
+ * struct builtin - contains a builtin string and related function
+ * @type: the builtin command flag
+ * @func: the function
+ */
+
+ typedef struct builtin
+ {
+	char *type;
+	int (*func)(info_t *);
+
+ } builtin_table;
+
+
+#endif
